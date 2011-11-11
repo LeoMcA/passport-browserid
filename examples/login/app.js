9a7ffec4bbfd6971c5bdf8e7838aed1db67c65a6
Leo McArdle <leomcardle@gmail.com>
2011-11-11 22:06:27 +0000

    Wowza! Got browserid working (though it's a MASSIVE hack)

--- examples/login/app.js
+++ examples/login/app.js
@@ -1,12 +1,13 @@
 var express = require('express')
   , passport = require('passport')
   , util = require('util')
-  , LocalStrategy = require('passport-local').Strategy;
+  , curl = require('../../lib/curl')
+  , BrowseridStrategy = require('../../lib/passport-browserid').Strategy;
   
 
 var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+    { id: 1, username: 'bob', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', email: 'joe@example.com' }
 ];
 
 function findById(id, fn) {
@@ -18,10 +19,10 @@ function findById(id, fn) {
   }
 }
 
-function findByUsername(username, fn) {
+function findByEmail(email, fn) {
   for (var i = 0, len = users.length; i < len; i++) {
     var user = users[i];
-    if (user.username === username) {
+    if (user.email === email) {
       return fn(null, user);
     }
   }
@@ -50,22 +51,24 @@ passport.deserializeUser(function(id, done) {
 //   credentials (in this case, a username and password), and invoke a callback
 //   with a user object.  In the real world, this would query a database;
 //   however, in this example we are using a baked-in set of users.
-passport.use(new LocalStrategy(
-  function(username, password, done) {
+passport.use(new BrowseridStrategy(
+  function(assertion, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
+      curl.curl("POST", "assertion="+assertion+"&audience=http://localhost:3000", "https://browserid.org/verify", function(data){
+        var data = JSON.parse(data);
+        if (data.status != "okay") { return done(null, false); }
       // Find the user by username.  If there is no user with the given
       // username, or the password is not correct, set the user to `false` to
       // indicate failure.  Otherwise, return the authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (user.password != password) { return done(null, false); }
-        return done(null, user);
-      })
-    });
-  }
+        findByEmail(data.email, function(err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          return done(null, user);
+        })
+      });
+    }
+  )}
 ));
 
 
@@ -109,7 +112,7 @@ app.get('/login', function(req, res){
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
+  passport.authenticate('browserid', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
